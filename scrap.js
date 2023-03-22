976928914cef43b16ccb07efcd8f84520b00d407
Katrina Huber-Juma <Katrina.huber@gmail.com>
2023-03-22 12:14:56 -0700

    practice with arrow fucntion syntax

---
+++ scrap.js
@@ -0,0 +1,24 @@
+let returnString = (params) => { "this is the codeblock" };
+undefined
+returnString('hey');
+undefined
+let returnString = (params) => "this is the codeblock";
+undefined
+returnString('hey');
+'this is the codeblock'
+let returnString = params => "this is the codeblock";
+undefined
+returnString('hey');
+'this is the codeblock'
+let returnString = () => "this is the codeblock";
+undefined
+returnString('hey');
+'this is the codeblock'
+let returnString = params => `this is the codeblock ${params}`;
+undefined
+returnString('hey');
+'this is the codeblock hey'
+let returnString = (params) => { return "this is the codeblock" };
+undefined
+returnString('hey');
+'this is the codeblock'
